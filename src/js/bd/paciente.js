6be4058501412e2c97d0a0e03ac6380ca8d49ba4
leider <leideryesidmm@gmail.com>
2023-07-05 16:57:37 -0500

    Perfil de paciente conexion

--- src/js/bd/paciente.js
+++ src/js/bd/paciente.js
@@ -1,21 +1,57 @@
-let servidorAPI="http://localhost:8081/"
+let servidorAPI="http://localhost:8081/";
+const cedula="1193098419";
 let listarPacientes= async()=>{
-    cedula="1193098419";
     const peticion= await fetch(servidorAPI+"paciente/findPacienteByCedula/"+cedula,{
       method:"GET",
       headers:{
         "Accept":"application/json",
         "Content-Type":"application/json"
       }
-    });
+});
     const paciente=await peticion.json();
-    console.log(paciente);
     document.getElementById("nombre").innerText=paciente.nombre;
     document.getElementById("fechaNacimiento").innerText=paciente.fechaNacimiento;
     document.getElementById("celular").innerText=paciente.celular;
     document.getElementById("direccion").innerText=paciente.direccion;
     document.getElementById("peso").innerText=paciente.peso;
     document.getElementById("altura").innerText=paciente.altura;
+    document.getElementById("eps").innerText=paciente.eps;
 }
 
-listarPacientes()
+let cuidadorActivo= async()=>{
+    const peticion= await fetch(servidorAPI+"paciente/cuidador/findCuidadorActivo/"+cedula,{
+      method:"GET",
+      headers:{
+        "Accept":"application/json",
+        "Content-Type":"application/json"
+      }
+    });
+
+    const cuidador=await peticion.json();
+   document.getElementById("nombreCuidador").innerText=cuidador.cuidador.nombre;
+    document.getElementById("dataTelefonoCuidador").innerText=cuidador.cuidador.telefono;
+    document.getElementById("dataDireccionCuidador").innerText=cuidador.cuidador.direccion;
+    document.getElementById("dataParentescoCuidador").innerText=cuidador.cuidador.parentesco.descripcion;
+}
+
+let alergias= async()=>{
+    const peticion= await fetch(servidorAPI+"paciente/alergia/listByPaciente/"+cedula,{
+      method:"GET",
+      headers:{
+        "Accept":"application/json",
+        "Content-Type":"application/json"
+      }
+    });
+
+    const alergias=await peticion.json();
+    console.log(alergias);
+    let msgalergias="";
+    
+    alergias.forEach(alergia => {
+        console.log(msgalergias)
+        msgalergias=msgalergias+alergia.alergia.nombre+", "
+    });
+    msgalergias = msgalergias.substring(0, msgalergias.length - 2);
+    
+   document.getElementById("dataAlergia").innerText=msgalergias;
+}
