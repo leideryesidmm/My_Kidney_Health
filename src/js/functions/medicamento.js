d5bd5098e2d7673e933ed09177a34b74d39e2e0e
leider <leideryesidmm@gmail.com>
2023-07-07 15:54:59 -0500

    correcion de medicamentos

--- src/js/functions/medicamento.js
+++ src/js/functions/medicamento.js
@@ -54,11 +54,31 @@
              '                   <p class="intervalo_tiempo" id="intervalo_tiempo">'+
              medicamento.intervaloTiempo+'                       </p>'+
              '               </div>'+
-             '               <div class="btn-editar-container">'+
-             '<a href="editarMedicamento.html" class="btn-editar"><img src="../img/editaraler.png"></a>'+
-             '<a href="medicamento.html" class="btn-inhabilitar" data-bs-toggle="modal"'+
-             '    data-bs-target="#inhabiltarMedicamento" id="btn-inhabilitar"><img src="../img/inhabilitarMed.png" alt=""></a></div>'+
-             '           </div>'+
+             '<div class="btn-editar-container">'+
+        '<a href="editarMedicamento.html" class="btn-editar"><img src="../img/editaraler.png"></a>'+
+        '<div><a href="" class="btn-inhabilitar" data-bs-toggle="modal"'+
+            'data-bs-target="#inhabilitarMedicamento" ><img src="../img/inhabilitar.png" alt="" id="inhabilitar"></a></div>'+
+            '</div>'+
+            '<div>'+
+                '<div class="modal" tabindex="-1" id="inhabilitarMedicamento">'+
+                    '<div class="modal-dialog">'+
+                        '<div class="modal-content">'+
+                            '<div class="modal-header">'+
+                                '<h5 class="modal-title">Eliminar Medicamento</h5>'+
+                              '  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>'+
+                            '</div>'+
+                            '<div class="modal-body">'+
+                                '<p>Esta seguro(a) de eliminar este medicamento:</p>'+
+                                '<label  class="medicamento" id="medicamento"></label>'+
+                           ' </div>'+
+                            '<div class="modal-footer">'+
+                                '<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>'+
+                                '<button type="submit" class="btn btn-danger">Eliminar</button>'+
+                           ' </div>'+
+                        '</div>'+
+                    '</div>'+
+                '</div>'+
+            '</div>'+
              '         </div>'+
              '       </div>'+
              '     </div>';
