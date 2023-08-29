c5c83187676125e80da3310635ac49f2e499b813
leider <leideryesidmm@gmail.com>
2023-08-29 15:10:24 -0500

    Prescripciones

--- src/js/functions/prescripcion.js
+++ src/js/functions/prescripcion.js
@@ -328,7 +328,7 @@ let mostrarPrescripcion= async (prescripcion, fecha, recambios) => {
                 if(fecha.getFullYear() === hoy.getFullYear() &&
                 fecha.getMonth() === hoy.getMonth() &&
                 fecha.getDate() === (hoy.getDate()-1)){
-                    msg+='background-color:rgb(252, 168, 59);">';
+                    msg+='background-color:rgb(252, 130, 59);">';
                     accion='<a style="color:black" href="agregarRecambio.html?idRecambio='+recambio.idRecambio+'"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">'
                     +'<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>'
                     +'<path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>'
@@ -346,10 +346,12 @@ let mostrarPrescripcion= async (prescripcion, fecha, recambios) => {
         +'            <h6 id="vertical">'
         +'                H'
         +'            </h6>'
-        +'            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-question-circle" viewBox="0 0 16 16">'
+        +'            <div class="tooltip-container">'+
+        '<span class="tooltip-text" data-toggle="tooltip" data-placement="top" title="H: Hecho&#10;P: Pendiente&#10;SH: Sin hacer">'+
+      '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-question-circle" viewBox="0 0 16 16">'
         +'                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>'
         +'                <path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z"/>'
-        +'              </svg>'
+        +'              </svg></span></div>'
         +'        </div>'   
         +'    </div>'
         msg+=datos1[cont];
